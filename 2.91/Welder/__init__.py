dc9bd45155e56ee9b0129eb42e4e9be2b001b3d5
JohnnieWooker <lukish14@wp.pl>
2025-03-17 15:10:11 +0100

    version name tweaked

--- 2.91/Welder/__init__.py
+++ 2.91/Welder/__init__.py
@@ -22,7 +22,7 @@ from . import panel
 bl_info = {
     "name": "Welder",
     "author": "Łukasz Hoffmann",
-    "version": (1,4,8),
+    "version": (1,4,9),
     "location": "View 3D > Object Mode > Tool Shelf",
     "wiki_url": "https://documentation.forgegon.com/Welder",
     "tracker_url": "https://documentation.forgegon.com/Welder",
